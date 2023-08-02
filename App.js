4d36586200b9c9544f59029b3ac898062b1b8c2a
Alexis Franco <ronaldofranco196@gmail.com>
2023-08-02 11:30:20 -0300

    inital commit

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Titulo from './components/Titulo';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Titulo/>
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/Titulo.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native"
+
+function Titulo() {
+    return (
+        <View>
+            <Text>
+                Control Financiero
+            </Text>
+        </View>
+    )
+}
+
+export default Titulo
